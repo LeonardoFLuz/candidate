96ceb3c4bb738069ba5d897666b7226f65cbe02e
Marcelolac <44480740+Marcelolac@users.noreply.github.com>
2018-11-09 15:06:06 -0200

    cadidate

--- src/main/java/br/edu/ulbra/election/candidate/repository/CandidateRepository.java
+++ src/main/java/br/edu/ulbra/election/candidate/repository/CandidateRepository.java
@@ -4,9 +4,7 @@ import br.edu.ulbra.election.candidate.model.Candidate;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CandidateRepository extends CrudRepository<Candidate, Long> {
-<<<<<<< HEAD
 	Candidate findByName(String name);
 	Candidate findByNumber(String number);
-=======
->>>>>>> 1ce85f053387a9175b39bb0ca775d8b616c1f1f5
+
 }
